05101f97c79fdad391a8a349c7c0e9ed9393a092
YdrMaster <ydrml@hotmail.com>
2020-04-28 11:14:32 +0800

    build: 复活，更新到最新的 gradle 和 kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "org.mechdancer"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    // 矩阵运算
+    implementation("org.mechdancer", "linearalgebra", "0.2.8-snapshot-3")
+
+    testImplementation("junit", "junit", "+")
+    testImplementation(kotlin("test-junit"))
+
+    // 支持网络工具
+    testImplementation(kotlin("reflect"))
+    testImplementation("org.mechdancer", "dependency", "+")
+    testImplementation("org.mechdancer", "remote", "+")
+    testImplementation("org.slf4j", "slf4j-api", "+")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+// 源码导出任务
+val sourceTaskName = "sourcesJar"
+task<Jar>(sourceTaskName) {
+    archiveClassifier.set("sources")
+    group = "build"
+
+    from(sourceSets["main"].allSource)
+}
+tasks["jar"].dependsOn(sourceTaskName)
+
+// 默认内联类
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-Xinline-classes")
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-Xinline-classes")
+}
+
+//publish {
+//    userOrg = 'mechdancer'
+//    groupId = 'org.mechdancer'
+//    artifactId = 'filters'
+//    publishVersion = '0.1.0'
+//    desc = 'filters kotlin utilities'
+//    website = 'https://github.com/MechDancer/filters'
+//}
